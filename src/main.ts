8996b85f34b9dae05e0a9f988c8c525b695fb24f
sheikhmahmudulhasanshium <sheikh.mahmudul.hasan.shium.aiub@gmail.com>
2025-10-13 01:57:06 +0600

    try-2 change upload limit

--- src/main.ts
+++ src/main.ts
@@ -21,8 +21,8 @@ async function bootstrap() {
     credentials: true,
   });
 
-  app.use(json({ limit: '50mb' }));
-  app.use(urlencoded({ extended: true, limit: '50mb' }));
+  app.use(json({ limit: '5mb' }));
+  app.use(urlencoded({ extended: true, limit: '5mb' }));
   app.setGlobalPrefix('api/v1');
 
   app.useGlobalPipes(
