7c9c249c2905be64a1ddc62e46563f3d4efda6cc
sheikhmahmudulhasanshium <sheikh.mahmudul.hasan.shium.aiub@gmail.com>
2025-10-24 00:43:15 +0600

    try to fix oauth

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -22,8 +22,7 @@ export default () => ({
     clientId: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     callbackUrl:
-      process.env.GOOGLE_CALLBACK_URL ||
-      'http://localhost:3001/auth/google/callback', // Update for production
+      process.env.GOOGLE_CALLBACK_URL || 'http://localhost:3000/auth/callback', // Update for production
   },
   r2: {
     endpoint: process.env.R2_ENDPOINT,
